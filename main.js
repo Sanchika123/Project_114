1665af5188ba1baee5a9382ab3fa7e2bbc1ba05e
Sanchika123 <79789552+Sanchika123@users.noreply.github.com>
2021-08-05 06:29:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas= createCanvas(400, 300);
+    canvas.center();
+}
+
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("downloaded_img.png");
+}
+
